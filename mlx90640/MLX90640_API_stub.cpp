5608bb6f589488158b96831dd59d9a0ff7520f4b
Stuart Holliday <pogsquog@hotmail.com>
2018-08-15 02:41:21 +0100

    now output random temps

--- mlx90640/MLX90640_API_stub.cpp
+++ mlx90640/MLX90640_API_stub.cpp
@@ -1,4 +1,5 @@
 
+#include <cstdlib>
 #include "MLX90640_API.h"
 
 int MLX90640_DumpEE(uint8_t slaveAddr, uint16_t *eeData)
@@ -18,6 +19,10 @@ int MLX90640_ExtractParameters(uint16_t *eeData, paramsMLX90640 *mlx90640)
 
 void MLX90640_CalculateTo(uint16_t *frameData, const paramsMLX90640 *params, float emissivity, float tr, float *result)
 {
+  for (int i=0; i<768; ++i)
+  {
+    result[i] = 255.0f * (float)rand() / (float)RAND_MAX;
+  }
 }
 
 float MLX90640_GetTa(uint16_t *frameData, const paramsMLX90640 *params)
